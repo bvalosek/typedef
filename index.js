d8be6221e070b78ad98a96a6f00b6408045fdb69
Brandon Valosek <bvalosek@gmail.com>
2014-01-13 14:20:21 -0600

    0.18.0

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  extends      : require('./lib/extends.js'),
+  mixin        : require('./lib/mixin.js'),
+  getArguments : require('./lib/getArguments.js'),
+  getName      : require('./lib/getName.js')
+
+};
+
+
