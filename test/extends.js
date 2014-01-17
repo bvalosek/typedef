d04364a070fd149361460725f7f06161e15f9d09
Brandon Valosek <bvalosek@gmail.com>
2014-01-17 17:32:16 -0600

    Tests for inherited members

--- test/extends.js
+++ test/extends.js
@@ -31,3 +31,27 @@ test('Operator instanceof semantics', function(t) {
   t.notOk(new Cousin() instanceof Child, 'cousin is not child');
 
 });
+
+test('Inherited member functions', function(t) {
+  t.plan(3);
+
+  function Parent() { }
+  Parent.prototype.foo = function() { return 'parent'; };
+
+  function Child() { }
+  extends_(Child, Parent);
+  Child.prototype.bar = function() { return 'child'; };
+
+  function Sibling() { }
+  extends_(Sibling, Parent);
+  function GrandChild() { }
+  extends_(GrandChild, Child);
+  function Cousin() { }
+  extends_(Cousin, Sibling);
+
+  t.strictEqual(new Parent().foo(), 'parent', 'parent method on parent');
+  t.strictEqual(new Child().foo(), 'parent', 'parent method on child');
+  t.strictEqual(new GrandChild().foo(), 'parent', 'parent method on grandchild');
+
+
+});
