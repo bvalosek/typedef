ff81a7f461def6946562c6058b6523ed153f8291
Brandon Valosek <bvalosek@gmail.com>
2014-01-17 18:00:58 -0600

    Arg order test

--- test/get-args.js
+++ test/get-args.js
@@ -2,16 +2,18 @@ var test    = require('tape');
 var getArgs = require('../lib/getArguments.js');
 
 test('Basic arguments', function(t) {
-  t.plan(4);
+  t.plan(5);
 
   function f() { }
   function g(a) { }
   function h(a, b) { }
   function i(a, b, c) { }
+  function j(c, b, a) { }
 
   t.deepEqual(getArgs(f), [], 'no args');
   t.deepEqual(getArgs(g), ['a'], '1 arg');
   t.deepEqual(getArgs(h), ['a', 'b'], '2 args');
   t.deepEqual(getArgs(i), ['a', 'b', 'c'], '3 args');
+  t.deepEqual(getArgs(j), ['c', 'b', 'a'], '3 args -- order');
 
 });
