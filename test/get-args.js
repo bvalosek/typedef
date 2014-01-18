a25caa1371131cd14ea4a1dcd383aac8db907d3c
Brandon Valosek <bvalosek@gmail.com>
2014-01-17 18:43:49 -0600

    White space testing

--- test/get-args.js
+++ test/get-args.js
@@ -17,3 +17,30 @@ test('Basic arguments', function(t) {
   t.deepEqual(getArgs(j), ['c', 'b', 'a'], '3 args -- order');
 
 });
+
+test('Weird whitespace arguments', function(t) {
+  t.plan(5);
+
+  function f(
+    ) { }
+  function g(   a  ) { }
+  function h(        a   ,
+    b) { }
+  function i( a  , b
+    ,
+    c
+    ) { }
+  function j(  c ,  b
+
+    ,
+
+
+    a                  ) { }
+
+  t.deepEqual(getArgs(f), [], 'no args');
+  t.deepEqual(getArgs(g), ['a'], '1 arg');
+  t.deepEqual(getArgs(h), ['a', 'b'], '2 args');
+  t.deepEqual(getArgs(i), ['a', 'b', 'c'], '3 args');
+  t.deepEqual(getArgs(j), ['c', 'b', 'a'], '3 args -- order');
+
+});
